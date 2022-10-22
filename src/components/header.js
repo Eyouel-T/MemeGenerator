07b94257fcc03d21007ed74b3ba1fa1c9b72f398
Eyouel <eeyoueltsegaye@gmail.com>
2022-10-23 00:04:25 +0300

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import troll from "../Troll Face.png"
+export default function Header(){
+    return (
+        <div className="header">
+            <img src={troll}/>
+            <h2>Meme Generator</h2>
+            <h4>Project 4</h4>
+        </div>
+    )
+}
---
+++ src/components/main.js
@@ -0,0 +1,12 @@
+export default function Main(){
+    return (
+        <div>
+            <form>
+                <input type="text" className="top" placeholder="Shutup"></input>
+                <input type="text" className="bottom" placeholder="and take my money"></input>
+                <br/>
+                <button type="submit" className="submitButton">Get a new meme image</button>
+            </form>
+        </div>
+    )
+}
